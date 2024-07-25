00746ef5b00b0d4e2c4bcbe86a8078c72e3113c9
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-25 22:08:47 +0300

    Login Function Completed

--- main.cpp
+++ main.cpp
@@ -8,19 +8,40 @@ const string users[2][2] = {{"Ahmed", "0123"},
 
 string login();
 
+bool confirm_user_data(const string &userData, short int index);
+
 int main() {
-    const string userId = login();
+    const string username = login();
+    cout << "\nUsername = " << username << '\n';
     return 0;
 }
 
 string login() {
-    string userId;
-    printf("Enter your username:");
-//    cin >> userId;
-//    short int trials = 0;
-//    while
-//    for (const auto user: users)
-//        if (user[0] == userId)
-//            return userId;
+    string username, password;
+    char counter = 0;
+    bool user_present_flag;
+    do {
+        printf("You have %d trial/s to\nEnter your username:", 3 - counter);
+        cin >> username;
+        ++counter;
+        user_present_flag = confirm_user_data(username, 0);
+    } while (!user_present_flag && counter < 3);
+    if (user_present_flag) {
+        counter = 0;
+        do {
+            printf("You have %d trial/s to\nEnter your password:", 3 - counter);
+            cin >> password;
+            ++counter;
+            if (confirm_user_data(password, 1))
+                return username;
+        } while (counter < 3);
+    }
     return "";
+}
+
+bool confirm_user_data(const string &userData, const short int index) {
+    for (auto user: users)
+        if (user[index] == userData)
+            return true;
+    return false;
 }
