7d1dedcc9696059483041fe5cb2b78ab6ecdec9f
Ahmed Madeh Noah <ahmed.madeh.noah@gmail.com>
2024-07-25 18:52:54 +0300

    Login Idea

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+const string users[2][2] = {{"Ahmed", "0123"},
+                            {"Noah",  "9876"}};
+
+string login();
+
 int main() {
-    cout << "Hello, World!!" << endl;
+    const string userId = login();
     return 0;
+}
+
+string login() {
+    string userId;
+    printf("Enter your username:");
+//    cin >> userId;
+//    short int trials = 0;
+//    while
+//    for (const auto user: users)
+//        if (user[0] == userId)
+//            return userId;
+    return "";
 }
